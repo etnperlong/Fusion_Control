d12c20dd691a77ad07e5ddbba7930fbd78a3dcbe
Vishal Dubey <yzvishal.vd@gmail.com>
2015-08-28 17:07:02 +0530

    v3: More fixes

--- app/src/main/java/vishal/vaf/fusioncontrol/LaunchActivity.java
+++ app/src/main/java/vishal/vaf/fusioncontrol/LaunchActivity.java
@@ -45,6 +45,132 @@ public class LaunchActivity extends AppCompatActivity {
                     }
                 }, 30L);
             }
+        } else if (resp.contains("Slide(0xAA)"))
+        {
+            yo = sharedPreferences.getString("right", null);
+            if (yo != null ) {
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Intent intent2 = packageManager.getLaunchIntentForPackage(yo);
+                        startActivity(intent2);
+                        finish();
+                    }
+                }, 30L);
+            }
+        } else if (resp.contains("Slide(0xBB"))
+        {
+            yo = sharedPreferences.getString("left", null);
+            if (yo != null ) {
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Intent intent2 = packageManager.getLaunchIntentForPackage(yo);
+                        startActivity(intent2);
+                        finish();
+                    }
+                }, 30L);
+            }
+        } else if (resp.contains("Slide(0xBA)"))
+        {
+            yo = sharedPreferences.getString("up", null);
+            if (yo != null ) {
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Intent intent2 = packageManager.getLaunchIntentForPackage(yo);
+                        startActivity(intent2);
+                        finish();
+                    }
+                }, 30L);
+            }
+        } else if (resp.contains("Slide(0x77)"))
+        {
+            yo = sharedPreferences.getString("w", null);
+            if (yo != null ) {
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Intent intent2 = packageManager.getLaunchIntentForPackage(yo);
+                        startActivity(intent2);
+                        finish();
+                    }
+                }, 30L);
+            }
+        } else if (resp.contains("Slide(0xAB)"))
+        {
+            yo = sharedPreferences.getString("down", null);
+            if (yo != null ) {
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Intent intent2 = packageManager.getLaunchIntentForPackage(yo);
+                        startActivity(intent2);
+                        finish();
+                    }
+                }, 30L);
+            }
+        } else if (resp.contains("Slide(0x63)"))
+        {
+            yo = sharedPreferences.getString("c", null);
+            if (yo != null ) {
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Intent intent2 = packageManager.getLaunchIntentForPackage(yo);
+                        startActivity(intent2);
+                        finish();
+                    }
+                }, 30L);
+            }
+        } else if (resp.contains("Slide(0x65)"))
+        {
+            yo = sharedPreferences.getString("e", null);
+            if (yo != null ) {
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Intent intent2 = packageManager.getLaunchIntentForPackage(yo);
+                        startActivity(intent2);
+                        finish();
+                    }
+                }, 30L);
+            }
+        } else if (resp.contains("Slide(0x6D)"))
+        {
+            yo = sharedPreferences.getString("m", null);
+            if (yo != null ) {
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Intent intent2 = packageManager.getLaunchIntentForPackage(yo);
+                        startActivity(intent2);
+                        finish();
+                    }
+                }, 30L);
+            }
+        } else if (resp.contains("Slide(0x6F)"))
+        {
+            yo = sharedPreferences.getString("o", null);
+            if (yo != null ) {
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Intent intent2 = packageManager.getLaunchIntentForPackage(yo);
+                        startActivity(intent2);
+                        finish();
+                    }
+                }, 30L);
+            }
         }
     }
 }
