ea36444f9b8370e7cdfae037f08f1c84b4409453
Vishal Dubey <yzvishal.vd@gmail.com>
2015-08-28 17:01:12 +0530

    v3: fix derp

--- app/src/main/java/vishal/vaf/fusioncontrol/LaunchActivity.java
+++ app/src/main/java/vishal/vaf/fusioncontrol/LaunchActivity.java
@@ -1,37 +1,50 @@
 package vishal.vaf.fusioncontrol;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+
+import vishal.vaf.fusioncontrol.checkutils.CheckUtils;
 
 public class LaunchActivity extends AppCompatActivity {
+
+    CheckUtils checkUtils;
+    PackageManager packageManager;
+    String yo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launch);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_launch, menu);
-        return true;
-    }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        checkUtils = new CheckUtils();
+        packageManager = this.getPackageManager();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        String resp = checkUtils.getResponse();
+
+        if (resp.contains("double click")) {
+            yo = sharedPreferences.getString("double_click", null);
+            if (yo != null) {
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Intent intent2 = packageManager.getLaunchIntentForPackage(yo);
+                        startActivity(intent2);
+                        finish();
+                    }
+                }, 30L);
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
